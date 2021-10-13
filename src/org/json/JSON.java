f0278df5f9e705eb0ee7c4dca0ffbb406e7689fd
Michael Peter Christen <mc@yacy.net>
2021-10-13 20:00:57 +0200

    json lib update

--- src/org/json/JSON.java
+++ src/org/json/JSON.java
@@ -29,14 +29,12 @@ class JSON {
      * Returns the input if it is a JSON-permissible value; throws otherwise.
      */
     static double checkDouble(double d) throws JSONException {
-        //if (Double.isInfinite(d) || Double.isNaN(d)) {
-        //    throw new JSONException("Forbidden numeric value: " + d);
-        //}
+        if (Double.isInfinite(d) || Double.isNaN(d)) {
+            throw new JSONException("Forbidden numeric value: " + d);
+        }
         return d;
     }
-
     static Boolean toBoolean(Object value) {
-        assert value != null;
         if (value instanceof Boolean) {
             return (Boolean) value;
         } else if (value instanceof String) {
@@ -49,9 +47,7 @@ class JSON {
         }
         return null;
     }
-
     static Double toDouble(Object value) {
-        assert value != null;
         if (value instanceof Double) {
             return (Double) value;
         } else if (value instanceof Number) {
@@ -64,9 +60,7 @@ class JSON {
         }
         return null;
     }
-
     static Integer toInteger(Object value) {
-        assert value != null;
         if (value instanceof Integer) {
             return (Integer) value;
         } else if (value instanceof Number) {
@@ -79,9 +73,7 @@ class JSON {
         }
         return null;
     }
-
     static Long toLong(Object value) {
-        assert value != null;
         if (value instanceof Long) {
             return (Long) value;
         } else if (value instanceof Number) {
@@ -94,9 +86,7 @@ class JSON {
         }
         return null;
     }
-
     static String toString(Object value) {
-        assert value != null;
         if (value instanceof String) {
             return (String) value;
         } else if (value != null) {
@@ -104,25 +94,24 @@ class JSON {
         }
         return null;
     }
-
     public static JSONException typeMismatch(Object indexOrName, Object actual,
             String requiredType) throws JSONException {
         if (actual == null) {
             throw new JSONException("Value at " + indexOrName + " is null.");
+        } else {
+            throw new JSONException("Value " + actual + " at " + indexOrName
+                    + " of type " + actual.getClass().getName()
+                    + " cannot be converted to " + requiredType);
         }
-        throw new JSONException("Value " + actual + " at " + indexOrName
-                + " of type " + actual.getClass().getName()
-                + " cannot be converted to " + requiredType);
-
     }
-
     public static JSONException typeMismatch(Object actual, String requiredType)
             throws JSONException {
         if (actual == null) {
             throw new JSONException("Value is null.");
+        } else {
+            throw new JSONException("Value " + actual
+                    + " of type " + actual.getClass().getName()
+                    + " cannot be converted to " + requiredType);
         }
-        throw new JSONException("Value " + actual
-                + " of type " + actual.getClass().getName()
-                + " cannot be converted to " + requiredType);
     }
 }
