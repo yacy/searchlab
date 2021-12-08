6d47826e0a6a80a30e0abea7d7583797cd7bfa6d
Michael Peter Christen <mc@yacy.net>
2021-12-08 16:25:31 +0100

    progress with dump index

--- src/net/yacy/grid/io/index/DumpIndex.java
+++ src/net/yacy/grid/io/index/DumpIndex.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -14,47 +15,42 @@ import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.InternalSettingsPreparer;
-import org.elasticsearch.node.Node;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.ByteBuffersDirectory;
+import org.apache.lucene.store.Directory;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 /**
  * A dump index provides a search result from a dump and holds it in-memory only.
  * The index is not available for write operations, all write operations to this
  * index is going into void.
  */
-@SuppressWarnings("deprecation")
 public class DumpIndex implements FulltextIndex {
 
     private String clusterName;
-    private Node node;
-    private Client elasticsearchClient;
+    Directory directory;
+    DirectoryReader indexReader;
+    IndexSearcher dirSearcher;
 
-    public DumpIndex(final String clusterName) {
+    public DumpIndex(final String clusterName) throws IOException {
         // create default settings and add cluster name
-        Settings.Builder settings = Settings.builder()
-                .put("cluster.routing.allocation.enable", "all")
-                .put("cluster.routing.allocation.allow_rebalance", "always")
-                .put("index.store.type", "mmapfs")
-                .put("http.enabled", "true")
-                .put("path.home", "elasticsearch-data");
-
         this.clusterName = clusterName;
-        if (this.clusterName != null) settings.put("cluster.name", this.clusterName);
-        this.node = new LocalNode(settings.build());
-        this.elasticsearchClient = this.node.client();
-    }
-
-    private static class LocalNode extends Node {
-        public LocalNode(Settings preparedSettings) {
-            super(InternalSettingsPreparer.prepareEnvironment(preparedSettings, null));
-        }
-
-        @Override
-        protected void registerDerivedNodeNameWithLogger(String nodeName) {
-        }
+        this.directory = new ByteBuffersDirectory();
+        this.indexReader = DirectoryReader.open(this.directory);
+        this.dirSearcher = new IndexSearcher(this.indexReader);
     }
 
     /**
@@ -62,16 +58,12 @@ public class DumpIndex implements FulltextIndex {
      * @param indexName
      * @param jsonlist
      */
-    public void load(String indexName, File jsonlist) throws IOException {
+    public void load(File jsonlist) throws IOException {
         InputStream is = new FileInputStream(jsonlist);
         if (jsonlist.getName().endsWith(".gz")) is = new GZIPInputStream(is);
         BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
 
-        StandardAnalyzer analyzer = new StandardAnalyzer();
-        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
-        /*
-        Directory d = getDirectory(indexName);
-        IndexWriter writter = new IndexWriter(d, indexWriterConfig);
+        List<Document> documents = new ArrayList<>();
         String line;
         while ((line = br.readLine()) != null) {
             try {
@@ -83,17 +75,21 @@ public class DumpIndex implements FulltextIndex {
                 String text = json.optString("text_t", "");
 
                 Document document = new Document();
-                document.add(new TextField("title", title, Store.YES));
-                document.add(new TextField("text", text, Store.YES));
-                writter.addDocument(document);
+                document.add(new StringField("sku", url , Store.YES));
+                document.add(new TextField("text_t", text , Store.YES));
+                document.add(new TextField("title", title , Store.YES));
+                documents.add(document);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
-        writter.close();
         br.close();
         is.close();
-        */
+        IndexWriter writer = new IndexWriter(this.directory, new IndexWriterConfig(new StandardAnalyzer()));
+        writer.addDocuments(documents);
+        writer.close();
+
+
     }
 
     @Override
@@ -190,22 +186,34 @@ public class DumpIndex implements FulltextIndex {
     public Query query(String indexName, YaCyQuery yq, YaCyQuery postFilter, Sort sort,
             WebMapping highlightField, int timezoneOffset, int from, int resultCount, int aggregationLimit, boolean explain,
             WebMapping... aggregationFields) {
-        // TODO Auto-generated method stub
+        QueryParser parser = new QueryParser("text_t", new StandardAnalyzer());
+        org.apache.lucene.search.Query query = null;
+        try {
+            query = parser.parse("hello");
+        } catch (ParseException e) {}
+        try {
+            TopDocs topDocs = this.dirSearcher.search(query, 10);
+            for (ScoreDoc scoreDoc: topDocs.scoreDocs) {
+                Document document = this.dirSearcher.doc(scoreDoc.doc);
+                String url = document.get("sku");
+                String text = document.get("text_t");
+                System.out.println("url:  " + url + "\ntext: " + text + "\n\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public static void main(String[] args) {
         File f = new File(args[0]);
-        /*
-        DumpIndex di = new DumpIndex();
         try {
-            di.load("test", f);
-
+            DumpIndex di = new DumpIndex(f.getName());
+            di.load(f);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("help: " + di.count("test", QueryBuilders.termQuery("text", "help")));
-        */
+
     }
 
 }
