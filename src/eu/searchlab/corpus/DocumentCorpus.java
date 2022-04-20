e9d85ffeb6237a2547d87cdaf9be0a9da3508c12
Michael Peter Christen <mc@yacy.net>
2022-04-20 14:30:48 +0200

    added document corpus stub

---
+++ src/eu/searchlab/corpus/DocumentCorpus.java
@@ -0,0 +1,11 @@
+package eu.searchlab.corpus;
+
+/**
+ * A document corpus is a set of documents that are defined by crawl starts.
+ * Therefore a set of crawl starts define a corpus.
+ */
+public class DocumentCorpus {
+
+
+
+}
