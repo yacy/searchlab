2ef58e5b436b70a9b544e864e76d1d9ac781c254
Michael Peter Christen <mc@yacy.net>
2022-09-11 22:09:52 +0200

    getIndexDocumentCountHistorgramPerMinute stub

--- src/net/yacy/grid/io/index/IndexDAO.java
+++ src/net/yacy/grid/io/index/IndexDAO.java
@@ -23,24 +23,59 @@ package net.yacy.grid.io.index;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import eu.searchlab.Searchlab;
 import eu.searchlab.storage.table.TimeSeriesTable;
 
 public class IndexDAO {
 
+	
+	public final static class TimeCount {long time; long count; public TimeCount(long time, long count) {this.time = time; this.count = count;}}
+	private final static Map<String, TimeCount> knownDocumentCount = new ConcurrentHashMap<>();
+	
     public static long getIndexDocumentCount(final String user_id) {
-        final String index_name = System.getProperties().getProperty("grid.elasticsearch.indexName.web", ElasticsearchClient.DEFAULT_INDEXNAME_WEB);
+    	long now = System.currentTimeMillis();
+    	final String index_name = System.getProperties().getProperty("grid.elasticsearch.indexName.web", ElasticsearchClient.DEFAULT_INDEXNAME_WEB);
         final long documents = Searchlab.ec.count(index_name, WebMapping.user_id_sxt.getMapping().name(), user_id);
+        knownDocumentCount.put(user_id, new TimeCount(now, documents));
         return documents;
     }
 
     public static TimeSeriesTable getIndexDocumentCountHistorgramPerMinute(final String user_id) {
+        long now = System.currentTimeMillis();
+        
+        // get a total index count for user
+        TimeCount tc = knownDocumentCount.get(user_id);
+        if (tc == null || tc.time < now - 600000) {
+        	getIndexDocumentCount(user_id);
+        	tc = knownDocumentCount.get(user_id);
+        }
+        
+        // get list of all documents that have been created in the last 10 minutes
+    	final String index_name = System.getProperties().getProperty("grid.elasticsearch.indexName.web", ElasticsearchClient.DEFAULT_INDEXNAME_WEB);
+        final Date fromDate = new Date(now - 600000);
+        String dateField = WebMapping.load_date_dt.getMapping().name();
+        final List<Map<String, Object>> documents = Searchlab.ec.queryWithCompare(index_name, WebMapping.user_id_sxt.getMapping().name(), user_id, dateField, fromDate);
+        
+        // aggregate the documents to counts per second
+        int[] counts = new int[600]; for (int i = 0; i < 600; i++) counts[i] = 0;
+        long offset = -1;
+        int offsetIndex = (int) ((tc.time - now) / 60000L);
+        for (Map<String, Object> map: documents) {
+        	Date date = (Date) map.get(dateField);
+        	if (date != null) counts[(int) ((now - date.getTime()) / 60000L)]++;
+        	
+        }
+        
+        // correct the counts using the time count info
+        
+        // make a time series
         final TimeSeriesTable tst = new TimeSeriesTable(new String[] {}, new String[] {}, new String[] {"data.documents"}, false);
-        final String index_name = System.getProperties().getProperty("grid.elasticsearch.indexName.web", ElasticsearchClient.DEFAULT_INDEXNAME_WEB);
-        final Date fromDate = new Date(System.currentTimeMillis() - 600000);
-        final List<Map<String, Object>> documents = Searchlab.ec.queryWithCompare(index_name, WebMapping.user_id_sxt.getMapping().name(), user_id, WebMapping.load_date_dt.getMapping().name(), fromDate);
-
+        for (int i = 599; i >= 0; i--) {
+        	int c = counts[i];
+        	tst.addValues((now - i) * 60000L, new String[0], new String[0], new long[] {c});
+        }
         return tst;
     }
 
